0acb7a9b0584cf7c458bf525ec0f201086ea2239
WalterDevIn <martinez.walter.mail@gmail.com>
2024-07-20 14:16:58 -0300

    omagad

---
+++ estructura.md
@@ -0,0 +1,2 @@
+##Categorias
+- vintage
--- main.js
+++ main.js
@@ -1,105 +1,79 @@
-// General, Mobile, AnimationSetter
+//Metodologia funcional
+const nav = document.querySelector("nav");
+const menuButton = document.createElement("button");
+const links = document.querySelector(".links");
+const dropdownLinks = document.querySelectorAll(".list");
+const header = document.querySelector("header");
 
-// la pantalla se mide y se aplincan cosas distintas
-
-class Page {
-
-    constructor() {
-        this.nav = document.querySelector("nav");
-        this.menuButton = document.createElement("button");
-        this.links = document.querySelector(".links");
-        this.dropdownLinks = document.querySelectorAll(".list");
-        this.header = document.querySelector("header");
-    }
-
-    createMenuButton() {
-        const button = document.createElement("button");
-        button.classList.add("mobile-nav")
-        button.innerHTML = `
-            <span class="line top"></span>
-            <span class="line middle"></span>
-            <span class="line bottom"></span>
-        `;
-        return button;
-    }
-
-    animateNav(scroll) {
-        if(scroll > this.header.offsetHeight * 1) {
-            this.nav.classList.remove("removing-nav");
-            this.nav.classList.add("fixed-nav");
-        } else {    
-            this.nav.classList.remove("fixed-nav")
-            this.nav.classList.add("removing-nav");
-        }
-    }
-
-    listenScroll() {
-        let scroll = window.scrollY;
-        this.animateNav(scroll);
-        document.addEventListener("scroll", (event) => {
-            scroll = window.scrollY;
-            this.animateNav(scroll);
-        })
-    }
-
-    unmountMobileNav() {
-        this.nav.classList.remove("mobile");
-        this.nav.remove(this.menuButton);
-    }
-
-    mountMobileNav() {
-        this.nav.classList.add("mobile");
-        this.nav.append(this.menuButton);
-    }
-
-    filterMaxWidthInChar(links) {
-        const charQuantity = [];
-        links.forEach(element => charQuantity.push(element.innerText.length))
-        const maxChars = Math.max(...charQuantity);
-        const widthCh = maxChars + 8;
-        return widthCh;
-    }
-
-    mountDropdownAnimations() {
-        this.dropdownLinks.forEach(element => {
-            const links = element.querySelectorAll("a");
-            const width = this.filterMaxWidthInChar(links);
-
-            links.forEach((subElement, index) => {
-                if(index == 0)
-                    subElement.classList.add("first");
-                if(index == links.length - 1) 
-                    subElement.classList.add("last")
-
-                subElement.style.setProperty("animation-delay", `${(index + 1) * 24}ms`)
-                subElement.style.setProperty("width", `${width}ch`)
-                subElement.style.setProperty("margin-left", `-${width / 2}ch`)
-            })
-        })
-    }
-
-    listenResize() {
-
-        document.addEventListener("resize", () => {
-            const width = window.innerWidth;
-            if(width < 600) this.mountMobileNav();
-            if(width > 600) this.unmountMobileNav();
-        });
-    }
-
-    main() {
-        this.mountDropdownAnimations();
-        
-        this.listenScroll();
-        this.listenResize();
-    }
-
-    static init() {
-        const page = new Page();
-        page.main();
-        return page;
-    }
-
+function createMenuButton() {
+    const button = document.createElement("button");
+    button.classList.add("mobile-nav")
+    button.innerHTML = `
+        <span class="line top"></span>
+        <span class="line middle"></span>
+        <span class="line bottom"></span>
+    `;
+    return button;
 }
 
-document.addEventListener("DOMContentLoaded", Page.init);
+function animateNav(scroll) {
+    if(scroll > header.offsetHeight * 1) {
+        nav.classList.remove("removing-nav");
+        nav.classList.add("fixed-nav");
+    } else {    
+        nav.classList.remove("fixed-nav")
+        nav.classList.add("removing-nav");
+    }
+}
+
+function listenScroll() {
+    let scroll = window.scrollY;
+    animateNav(scroll);
+    document.addEventListener("scroll", (event) => {
+        scroll = window.scrollY;
+        animateNav(scroll);
+    })
+}
+
+function unmountMobileNav() {
+    this.nav.classList.remove("mobile");
+    this.nav.remove(this.menuButton);
+}
+
+function filterMaxWidthInChar(links) {
+    const charQuantity = [];
+    links.forEach(element => charQuantity.push(element.innerText.length))
+    const maxChars = Math.max(...charQuantity);
+    const widthCh = maxChars + 8;
+    return widthCh;
+}
+
+function mountDropdownAnimations() {
+    dropdownLinks.forEach(element => {
+        const links = element.querySelectorAll("a");
+        const width = filterMaxWidthInChar(links);
+
+        links.forEach((subElement, index) => {
+            if(index == 0)
+                subElement.classList.add("first");
+            if(index == links.length - 1) 
+                subElement.classList.add("last")
+
+            subElement.style.setProperty("animation-delay", `${(index + 1) * 24}ms`)
+            subElement.style.setProperty("width", `${width}ch`)
+            subElement.style.setProperty("margin-left", `-${width / 2}ch`)
+        })
+    })
+}
+
+function listenResize() {
+    document.addEventListener("resize", () => {
+        const width = window.innerWidth;
+        if(width < 600) mountMobileNav();
+        if(width > 600) unmountMobileNav();
+    });
+}
+
+mountDropdownAnimations(); 
+listenScroll();
+listenResize();
